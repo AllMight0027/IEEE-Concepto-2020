c7a44346a2382648f0c7d6e0398767d8565973d6
Shaurya Bhandari <shauryabhandari25@gmail.com>
2020-09-17 20:39:55 +0530

    basic site readt

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,26 +1,32 @@
 import React from "react";
 import "./Home.css";
-import logo from "./Concepto_White.svg";
+import logo from "./logo.webm";
 import Timer from "../timer/Timer";
 import Speakers from "../Speakers/Speakers";
 import Footer from "../Footer/Footer";
 
 const Home = () => {
-    return (
-        <div className="text-center">
-            <img src={logo} className="logo" alt="Concepto Logo" />
-            <br />
-            <div className="timer">
-                <Timer />
-            </div>
-            <button className="btn btn-outline-light">GET YOUR PASSES</button>
-            <div className="speakers">
-                <h1>Our Speakers</h1>
-                <Speakers />
-            </div>
-            <Footer />
-        </div>
-    );
+  return (
+    <div className="text-center">
+      {/* <img src={logo} className="logo" alt="Concepto Logo" /> */}
+      <video src={logo} width="300px" className="logo" autoPlay="true"></video>
+      <br />
+      <div className="timer">
+        <Timer />
+      </div>
+      <button
+        className="btn btn-outline-light text-center"
+        href="www.google.com"
+      >
+        GET YOUR PASSES
+      </button>
+      <div className="speakers">
+        <h1>Our Speakers</h1>
+        <Speakers />
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default Home;
