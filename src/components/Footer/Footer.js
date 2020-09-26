06e40e3b3356865588c0bd9f3455a4783e6a64ac
Pratyaksh Saxena <pratyakshsaxena88@gmail.com>
2020-09-27 00:38:46 +0530

    footer changes

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,12 +3,87 @@ import "./footer.css";
 export default function Footer() {
   return (
     <footer>
+      <div className="pt-2">
+        <div className="container">
+          <div className="row">
+            <div className="col-12 text-center">Follow Us</div>
+            <div className="col-12 text-center text-white pb-3">
+              {" "}
+              <a
+                href="https://www.linkedin.com/company/ieeesrmist/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  src="https://img.icons8.com/ios/50/ffffff/linkedin.png"
+                  className="mr-1"
+                  style={{ maxHeight: "38px", maxWidth: "38px" }}
+                />
+              </a>
+              <a
+                href="https://twitter.com/ieeesrmist"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  src="https://img.icons8.com/ios/50/ffffff/twitter-circled.png"
+                  className="mr-1"
+                  style={{ maxHeight: "38px", maxWidth: "38px" }}
+                />
+              </a>
+              <a
+                href="https://www.instagram.com/ieeesrmist/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  src="https://img.icons8.com/ios/50/ffffff/instagram-new.png"
+                  className="mr-1"
+                  style={{ maxHeight: "38px", maxWidth: "38px" }}
+                />
+              </a>
+              <a
+                href="https://www.medium.com/ieeesrmist/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  src="https://img.icons8.com/ios/50/ffffff/medium.png"
+                  className="mr-1"
+                  style={{ maxHeight: "38px", maxWidth: "38px" }}
+                />
+              </a>
+              <a
+                href="https://github.com/IEEE-SRM-Student-Branch"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  src="https://img.icons8.com/ios/50/ffffff/github.png"
+                  className="mr-1"
+                  style={{ maxHeight: "38px", maxWidth: "38px" }}
+                />
+              </a>
+              <a
+                href="https://www.facebook.com/ieeesrmist"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  src="https://img.icons8.com/ios/50/ffffff/facebook.png"
+                  style={{ maxHeight: "38px", maxWidth: "38px" }}
+                />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
       <div className="footer pt-2">© 2020 Made with ❤️️ by IEEE SRM SB</div>
       <div className="priv">
         <ul className="list-unstyled d-flex justify-content-center mb-0">
           <li className="mr-2 mb-2">
             <a
-              className="text-white"
+              className="text-muted"
               href="https://www.ieeesrmist.in/disclaimer"
               target="_blank"
             >
@@ -17,7 +92,7 @@ export default function Footer() {
           </li>
           <li className="mr-2 mb-2">
             <a
-              className="text-white"
+              className="text-muted"
               href="https://www.ieeesrmist.in/codeofconduct"
               target="_blank"
             >
@@ -26,7 +101,7 @@ export default function Footer() {
           </li>
           <li className="mr-2 mb-2">
             <a
-              className="text-white"
+              className="text-muted"
               href="https://www.ieeesrmist.in/privacypolicy"
               target="_blank"
             >
