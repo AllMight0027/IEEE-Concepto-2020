cc754d06908e54b0f3f55c3f7ec08a88db3da1cd
Snehil <snehilwebber@gmail.com>
2020-09-30 02:25:55 +0530

    add telegram group modal

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import "./App.css";
 import Home from "./components/home/Home";
@@ -7,6 +7,27 @@ import Events from "./components/events/Events";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
+  const [modalShowing, setModalShowing] = useState("hide");
+  const [modalDisplayCSS, setModalDisplayCSS] = useState("none");
+
+  useEffect(() => {
+    const visited = localStorage.getItem("visited");
+    if (!visited) {
+      localStorage.setItem("visited", true);
+      setModalDisplayCSS("block");
+      setTimeout(() => {
+        setModalShowing("show");
+      }, 500);
+    }
+  }, []);
+
+  const handleModalClose = () => {
+    setModalShowing("hide");
+    setTimeout(() => {
+      setModalDisplayCSS("none");
+    }, 200);
+  };
+
   return (
     <div className="App">
       <Switch>
@@ -17,6 +38,41 @@ function App() {
           <Events />
         </Route>
       </Switch>
+      <div
+        className={`modal fade ${modalShowing}`}
+        tabindex="-1"
+        style={{ display: modalDisplayCSS }}
+      >
+        <div className="modal-dialog modal-dialog-centered">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title">Get in touch!</h5>
+              <button
+                type="button"
+                className="close"
+                onClick={handleModalClose}
+                aria-label="Close"
+              >
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div className="modal-body">
+              Join our Discord server to get the latest updates
+            </div>
+            <div className="modal-footer">
+              <a
+                type="button"
+                className="btn btn-telegram"
+                onClick={handleModalClose}
+                href="http://t.me/concepto2020"
+                target="_blank"
+              >
+                Join Telegram Group
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
